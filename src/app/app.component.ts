88924142476994ce6a1af77ca1a1f5004e4106d0
ram-chadar <javaprojectwork2715@gmail.com>
2024-12-04 09:29:34 +0530

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SubMenu';
+
+  MENU_ITEMS = [
+    {
+      label: 'Action',
+      link: '#',
+      children: [],
+    },
+    {
+      label: 'Another action',
+      link: '#',
+      children: [],
+    },
+    {
+      label: 'Submenu',
+      link: '#',
+      children: [
+        {
+          label: 'Submenu item 1',
+          link: '#',
+          children: [],
+        },
+        {
+          label: 'Submenu item 2',
+          link: '#',
+          children: [],
+        },
+        {
+          label: 'Submenu item 3',
+          link: '#',
+          children: [
+            {
+              label: 'Multi level 1',
+              link: '#',
+              children: [],
+            },
+            {
+              label: 'Multi level 2',
+              link: '#',
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  handleClick(menuName: string) {
+    alert(`You clicked on: ${menuName}`);
+  }
+
+  toggleSubmenu(event: MouseEvent, isVisible: boolean): void {
+    const target = event.target as HTMLElement;
+    const submenu = target.nextElementSibling as HTMLElement;
+
+    if (submenu && submenu.classList.contains('dropdown-submenu')) {
+      // Toggle the display property for the submenu
+      submenu.style.display = isVisible ? 'block' : 'none';
+    }
+  }
+}
